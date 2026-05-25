106fad21d77663a7e15658a759ed78b8ab801784
RANGO <artur.detistov@bk.ru>
2026-05-25 15:59:52 +0300

    MainPage.tsx

--- coderDecoderReact/src/pages/MainPage.tsx
+++ coderDecoderReact/src/pages/MainPage.tsx
@@ -6,11 +6,10 @@ const MainPage = () => {
   const shifr = () => {
     let word = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю','я'];
     let num = [12, 3, 65, 7, 1, 0, 13, 44, 4, 6, 80, 21, 22, 23, 61, 89, 98, 198, 101, 51, 32, 52, 76, 66, 55, 50, 9, 27, 28, 158, 161, 133, 206];
-    let c = text;
     let doc;
     let result = "";
-    doc = c.split("")
-    for (let i = 0; i < c.length; i++){
+    doc = text.split("")
+    for (let i = 0; i < text.length; i++){
         for (let j = 0; j < word.length; j++){
             if (doc[i] == word[j]){
                 result += num[j] + " "
@@ -22,11 +21,10 @@ const MainPage = () => {
   const deshifr = () => {
     let word = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю','я'];
     let num = [12, 3, 65, 7, 1, 0, 13, 44, 4, 6, 80, 21, 22, 23, 61, 89, 98, 198, 101, 51, 32, 52, 76, 66, 55, 50, 9, 27, 28, 158, 161, 133, 206];
-    let c = res;
     let doc;
     let result = "";
-    doc = c.split(" ")
-    for (let i = 0; i < c.length; i++){
+    doc = res.split(" ")
+    for (let i = 0; i < res.length; i++){
         for (let j = 0; j < word.length; j++){
             if (Number(doc[i]) == num[j]){
                 result += word[j] + ""
